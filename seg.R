27c411b97f8b257e4e2b89c6b7e85fd1534f81f3
SRenan <renan.sauteraud@gmail.com>
2013-04-23 09:47:45 -0700

    Multiple chr enabled

--- seg.R
+++ seg.R
@@ -1,13 +1,49 @@
-RCPP<-function(data, minReads=2, minDist=0, step=20, minLength=100, width=250){
+RCPP<-function(data, dataC=NULL, minReads=2, minDist=0, step=20, minLength=100, width=250, verbose=2){
   #minDist = 25 in PING
-  #data is GRanges with 1chr
+  .checkInput(data, dataC)
   
   maxStep=0;
 
-  ##TODO: Later, make a list of yF and a list of yR. For chrs. (add a char vector for chrnames)
-  yF<-start(data[strand(data)=="+"])
-  yR<-end(data[strand(data)=="-"])
-  #resList<-.Call("CPP_seg", yF, yR)
-  resList<-CPP_seg(yF, yR, minReads, step, width, maxStep, minLength, minDist)
+
+  #if(is.null(dataC)){
+    #cF
+  #} else{
+    #cF<-start(data[strand(dataC)=="+"])
+    #cR<-end(data[strand(dataC)=="-"])
+  #}
+  #cList<-list(list("cF"=cF, "cR"=cR))
+  dList<-.format4cpp(data)
+  cList<-.format4cpp(dataC)
+  resList<-CPP_seg(dList, cList, minReads, step, width, maxStep, minLength, minDist, verbose)
+  #resList<-CPP_seg(yF, yR, minReads, step, width, maxStep, minLength, minDist)
   return(resList)
+}
+
+#Prepare a GRanges object to be passed to CPP_seg
+.format4cpp<-function(GRobject){
+  chrs<-levels(seqnames(GRobject))
+  ret<-vector('list', length(chrs))
+  names(ret) <- chrs
+  for(chr in chrs){
+    GRchr<-GRobject[seqnames(GRobject)==chr]
+    ret[[chr]]<-vector('list',2)
+    ret[[chr]][["F"]]<-start(GRchr[strand(GRchr)=="+"])
+    ret[[chr]][["R"]]<-end(GRchr[strand(GRchr)=="-"])
+  }
+  return(ret)
+}
+
+.checkInput<-function(data, dataC){
+  if(!is(data, "GRanges")){
+    stop("The input data should be an object of class 'GRanges'. You provided: ", class(data))
+  }
+  if(!is.null(dataC)){
+    if(!is(dataC, "GRanges") ){
+      stop("The input dataC should be an object of class 'GRanges' or NULL. You provided: ", class(dataC))
+    }
+    if(length(levels(seqnames(dataC))) != length(levels(seqnames(data)))){
+      stop("The data and control do not have the same number of chromosomes.\n",
+        "data: ", length(levels(seqnames(data))), "  dataC: ", length(levels(seqnames(dataC))))
+    }
+  }
 }
--- seg.cpp
+++ seg.cpp
@@ -1,3 +1,8 @@
+/**
+* TODO:
+*   callRegionsL
+*   add mappability profile and control reads. Used only in segR
+*/
 #include <Rcpp.h>
 #include <iostream>
 
@@ -17,48 +22,69 @@ struct region{ //The regions
   vector<int> readsR;
 };
 
-void getWindowsScores(vector<window>& windows, vector<int>& yyF, vector<int>& yyR, int width, int minDist);
+SEXP seg_chr(vector<int>& yF, vector<int>& yR, vector<int>& contF, vector<int>& contR, int minReads, int step, int width, int maxStep, int minLength, int minDist, int verbose);
+void getWindowsScores(vector<window>& windows, vector<int>& yF, vector<int>& yR, int width, int minDist);
 int callRegions(vector<window>& windows, int width, int minReads, vector<region>& regions);
-void segR(vector<int> yyF, vector<int>yyR, vector<window>& windows, vector<region>& regions);
-
-
+void segR(vector<int> yF, vector<int>yR, vector<window>& windows, vector<region>& regions);
 
 //[[Rcpp::export]]
-SEXP CPP_seg(SEXP yF, SEXP yR, int minReads, int step, int width, int maxStep, int minLength, int minDist){ //yF=dataF
+SEXP CPP_seg(Rcpp::List data, Rcpp::List dataC, int minReads, int step, int width, int maxStep, int minLength, int minDist, int verbose){
 //minReads : The min number of reads for a window to be included in a region
-//step     : The length of the sliding window
-//width    : The max length of the regions
+//step     : The step for the sliding window
+//width    : Half the length of the sliding window
 //maxStep  : Used in callRegionsL
 //minLength: Used in callRegionsL
 //minDist  : in PING, when reads are too close to the center of the windows, they are not counted in the window score
+//verbose  : Print debug values
+  string chr;
+  vector<string> chrs =  data.names();
 
-  int i;
-  int verbose = 2;
+  Rcpp::List ret(data.size());
 
-  //To c++ vectors
-  std::vector<int> yyF = Rcpp::as<std::vector<int> >(yF);
-  std::vector<int> yyR = Rcpp::as<std::vector<int> >(yR);
+  for(int i=0; i < data.size(); i++){
+    chr = chrs[i];
+    if(verbose>1){
+      cout<<"Processing chromosome "<<chr<<endl;
+    }
+    //access sub list
+    Rcpp::List dataList = data[chr];
+    Rcpp::List contList = dataC[chr];
+    //R to c++ vectors
+    std::vector<int> yF = Rcpp::as<std::vector<int> >(dataList["F"]);
+    std::vector<int> yR = Rcpp::as<std::vector<int> >(dataList["R"]);
+    std::vector<int> contF = Rcpp::as<std::vector<int> >(contList["F"]);
+    std::vector<int> contR = Rcpp::as<std::vector<int> >(contList["R"]);
+    ret[i] = seg_chr(yF, yR, yF, yR, minReads, step, width, maxStep, minLength, minDist, verbose);
+  }
+  ret.names() = chrs;
+  return(Rcpp::wrap(ret));
+}
+
+
+SEXP seg_chr(vector<int>& yF, vector<int>& yR, vector<int>& contF, vector<int>& contR, int minReads, int step, int width, int maxStep, int minLength, int minDist, int verbose){
+
+  std::sort(yF.begin(), yF.end());
+  std::sort(yR.begin(), yR.end());
+  if(length(contF)>0 && length(contR)>0){
+    sort(contF.begin(), contF.end());
+    sort(contR.begin(), contR.end());
+  }
   
-  int nF = yyF.size();
-  int nR = yyR.size();
-
-  //sort yyF & yyR
-  std::sort(yyF.begin(), yyF.end());
-  std::sort(yyR.begin(), yyR.end());
-  int m=std::min(yyF.at(0), yyR.at(0));
-  int M=std::max(yyF.at(nF-1), yyR.at(nR-1));
+  int m=std::min(yF.at(0), yR.at(0));
+  int M=std::max(yF.at(yF.size()-1), yR.at(yR.size()-1));
 
   int nWindows = (M-m)/step; //Number of windows
 
   vector<window> windows(nWindows);
+  int i;
   for(i=0; i<nWindows; i++){
     windows.at(i).center=m+i*step;
   }
 
-  //Vector for the bigger regions (merged adjacent windows)
+  //Vector of the maximal possible size for the bigger regions (merged adjacent windows)
   std::vector<region> regions((M-m)/(2*width));
 
-  getWindowsScores(windows, yyF, yyR, width, minDist); //Sliding window
+  getWindowsScores(windows, yF, yR, width, minDist); //Sliding window
   int nRegions;
   if(maxStep==0){
     nRegions = callRegions(windows, width, minReads, regions); //Get the actual regions
@@ -74,12 +100,12 @@ SEXP CPP_seg(SEXP yF, SEXP yR, int minReads, int step, int width, int maxStep, i
   }
 
   if(nRegions>0){
-    segR(yyF, yyR, windows, regions);
+    segR(yF, yR, windows, regions);
   }
   else{
   }
 
-  vector<vector<int> > Freads(nRegions);
+  vector<vector<int> > Freads(nRegions); //vector of vectors
   vector<vector<int> > Rreads(nRegions);
 
   for(int idxRet = 0; idxRet < regions.size(); ++idxRet){
@@ -99,31 +125,31 @@ SEXP CPP_seg(SEXP yF, SEXP yR, int minReads, int step, int width, int maxStep, i
 *   F reads on the left within width and R on the right within width
 *   Total length of the window = 2*width
 */
-void getWindowsScores(vector<window>& windows, vector<int>& yyF, vector<int>& yyR, int width, int minDist){
+void getWindowsScores(vector<window>& windows, vector<int>& yF, vector<int>& yR, int width, int minDist){
   int i; //Read index iterator
   int startF=0, startR=0; //The read index for the next window
   int nbF=0, nbR=0; //Number of reads for one window
-  int nF=yyF.size(), nR=yyR.size();
+  int nF=yF.size(), nR=yR.size();
 
   std::vector<window>::iterator it;
   for(it = windows.begin(); it != windows.end(); ++it){
     nbF=0;
     i=startF;
-    while((i<nF) && ((it->center-yyF.at(i))>width)){
+    while((i<nF) && ((it->center-yF.at(i))>width)){
       i++;
     }
     startF=i;
-    while((i<nF) && ((it->center-yyF.at(i))<=width) && ((it->center-yyF.at(i))>=minDist)){ //Fwd reads in the winow
+    while((i<nF) && ((it->center-yF.at(i))<=width) && ((it->center-yF.at(i))>=minDist)){ //Fwd reads in the winow
       nbF++;
       i++;
     }
     nbR=0;
     i=startR;
-    while((i<nR) && ((yyR.at(i)-it->center)<minDist)){  //Rev reads that are out of the window on the left (before the window center)
+    while((i<nR) && ((yR.at(i)-it->center)<minDist)){  //Rev reads that are out of the window on the left (before the window center)
       i++;
     }
     startR=i;
-    while((i<nR) && ((yyR.at(i)-it->center)<=width) && ((yyR.at(i)-it->center)>=minDist)){ //Rwd reads in the winow
+    while((i<nR) && ((yR.at(i)-it->center)<=width) && ((yR.at(i)-it->center)>=minDist)){ //Rwd reads in the winow
       nbR++;
       i++;
     }
@@ -182,9 +208,9 @@ int callRegions(vector<window>& windows, int width,int minReads, vector<region>&
 /* Find which reads belongs to which region
 *  TODO: The reads will change when I add map & control
 */
-void segR(vector<int> yyF, vector<int>yyR, vector<window>& windows, vector<region>& regions){
+void segR(vector<int> yF, vector<int>yR, vector<window>& windows, vector<region>& regions){
   int minLoc, maxLoc; //tmp save regions boundaries
-  int nF=yyF.size(), nR=yyR.size();
+  int nF=yF.size(), nR=yR.size();
   int nRegions=regions.size();
   
   int startF=0, endF=0;
@@ -195,28 +221,28 @@ void segR(vector<int> yyF, vector<int>yyR, vector<window>& windows, vector<regio
     minLoc = it->start; 
     maxLoc = it->end;
     
-    //subset yyF & yyR based on the start & end of the regions
-    while(yyF[startF]<minLoc){
+    //subset yF & yR based on the start & end of the regions
+    while(yF[startF]<minLoc){
       startF++;
     }
     endF=startF;
-    while(yyF[endF]<maxLoc){
+    while(yF[endF]<maxLoc){
       endF++;
     }
     endF--;
-    while(yyR[startR]<minLoc){
+    while(yR[startR]<minLoc){
       startR++;
     }
     endR=startR;
-    while(yyR[endR]<maxLoc){
+    while(yR[endR]<maxLoc){
       endR++;
     }
     endR--;
 
     vector<int> regionF(endF-startF);
     vector<int> regionR(endR-startR);
-    std::copy(yyF.begin()+startF, yyF.begin()+endF, regionF.begin());
-    std::copy(yyR.begin()+startR, yyR.begin()+endR, regionR.begin());
+    std::copy(yF.begin()+startF, yF.begin()+endF, regionF.begin());
+    std::copy(yR.begin()+startR, yR.begin()+endR, regionR.begin());
 
     it->readsF = regionF;
     it->readsR = regionR;
@@ -224,5 +250,11 @@ void segR(vector<int> yyF, vector<int>yyR, vector<window>& windows, vector<regio
     //Regions do not overlap
     startF = endF;
     startR = endR;
+
+    //process control data
+    if(contF.size()>0 && contR.size()>0){
+
+    }
+    
   }
 }
